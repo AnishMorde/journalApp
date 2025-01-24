60f36e52d69375c5fbef9fb73dcdb3bd14cb0047
AnishMorde <anishmorde@gmail.com>
2025-01-24 11:26:39 +0530

    new

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -3,7 +3,7 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//main programme
 @SpringBootApplication
 public class JournalApplication {
 
